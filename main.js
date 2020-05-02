0c5335b11b9138f4af488e71a476b51da5123d2a
Herd Design <63194593+herd-design@users.noreply.github.com>
2020-05-02 14:42:51 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,50 @@
+$(window).on('load', function () {
+		$(".loader").delay(300).fadeOut("slow");
+	});
+
+	$(document).ready(function () {
+		new WOW().init();
+		$('#fullpage').fullpage({
+			autoScrolling: false,
+			fitToSection: false,
+			verticalCentered: true,
+		});
+		$(".product-slider").slick({
+			dots: false,
+			arrows:true,
+			prevArrow: '<div class="slick-prev"><img src="assets/images/arrow-right.svg" alt=""/></div>',
+			nextArrow: '<div class="slick-next"><img src="assets/images/arrow-left.svg" alt=""/></div>',
+			slidesToShow: 3,
+			slidesToScroll: 1,			
+			autoplay:true,
+			responsive: [
+				{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+				},
+				{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+				},
+			]				
+		});	
+		$(".wrp-listtestimoni").slick({
+			dots: false,
+			arrows:true,
+			prevArrow: '<div class="slick-prev"><img src="assets/images/arrowtestimoni-left.svg" alt=""/></div>',
+			nextArrow: '<div class="slick-next"><img src="assets/images/arrowtestimoni-right.svg" alt=""/></div>',
+			slidesToShow: 1,
+			slidesToScroll: 1,		
+			autoplay:false		
+		});	
+		$(".navmenu").click(function(){
+			$(this).toggleClass("active");
+			$(".mainmenu").toggleClass("active");
+		});	
+	});
